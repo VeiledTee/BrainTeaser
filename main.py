9a41018999017606818772b993ad1f2c9ff76925
penguins <ethanheavey@outlook.com>
2023-11-23 07:35:37 -0400

    testing

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 model_name: str = 'roberta-large'
 # model_name: str = 'Phind/Phind-CodeLlama-34B-v2'
 
-
 def answer_MCQ(prompt: str, options: list[str]) -> str:
     nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
     QA_context: str = '\n'.join(options)
@@ -27,18 +26,18 @@ def answer_MCQ(prompt: str, options: list[str]) -> str:
 
 
 if __name__ == '__main__':
-    train_data = np.load('data/WP-train.npy', allow_pickle=True)
-    for i, d in enumerate(train_data):
+    data = np.load('data/WP-train.npy', allow_pickle=True)
+    for i, d in enumerate(data):
         if i < 3:
             test_prompt: str = f"Answer the following brainteaser. Only choose one of the answers listed:" \
-                               f"Question: {train_data[i]['question']}" \
+                               f"Question: {data[i]['question']}" \
                                # f"Answers: {data[i]['choice_list']}"
-            print(train_data[i]['question'])
-            print('\n'.join(train_data[i]['choice_list']))
-            valid_data = np.load('data/WP_eval_data_for_practice.npy', allow_pickle=True)
-            # print(valid_data[i]['question'])
-            # print('\n'.join(valid_data[i]['choice_list']))
-            answer: str = answer_MCQ(prompt=test_prompt, options=train_data[i]['choice_list'])
-            print(f"Q: {train_data[i]['question']}\nOptions: {train_data[i]['choice_list']}\nCorrect: {train_data[i]['answer']}\nA: {answer['answer'].strip()}\n=====")
+            # print(data[0]['question'])
+            # print('\n'.join(data[0]['choice_list']))
+            # data = np.load('data/WP_eval_data_for_practice.npy', allow_pickle=True)
+            # print(data[0])
+            # print(generate_one_completion())
+            answer: str = answer_MCQ(prompt=test_prompt, options=data[i]['choice_list'])
+            print(f"Q: {data[i]['question']}\nOptions: {data[i]['choice_list']}\nCorrect: {data[i]['answer']}\nA: {answer['answer'].strip()}\n=====")
         else:
             break
