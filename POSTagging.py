2a794a17219389898af2876bf0ca13453878e0b1
Veiled Tee <ethanheavey@outlook.com>
2024-01-25 10:32:20 -0400

    POS Tagging

---
+++ POSTagging.py
@@ -0,0 +1,31 @@
+import nltk
+from nltk.tag import StanfordPOSTagger
+from nltk.tokenize import word_tokenize
+
+
+class SimplePOSTagger:
+    def __init__(self):
+        # Set the path to the Stanford POS Tagger JAR file and model file
+        self.stanford_pos_jar = 'POS/stanford-postagger.jar'
+        self.stanford_pos_model = 'POS/english-bidirectional-distsim.tagger'
+
+        # Initialize the Stanford POS Tagger with the java_home parameter
+        self.stanford_tagger = StanfordPOSTagger(self.stanford_pos_model, self.stanford_pos_jar,
+                                                 java_options=f'-Xmx1024m', encoding='utf8')
+
+    def tag_sentence(self, sentence):
+        # Tokenize the sentence
+        tokens = word_tokenize(sentence)
+
+        # Perform POS tagging
+        pos_tags = self.stanford_tagger.tag(tokens)
+
+        # Print the POS tags
+        for word, pos_tag in pos_tags:
+            print(f"{word}: {pos_tag}")
+
+    def filter_objects(self, tagged_sentence):
+        # Identify potential objects based on nouns (NN, NNS, NNP, NNPS) and verbs (VB, VBD, VBG, VBN, VBP, VBZ)
+        potential_objects = [word for word, pos in tagged_sentence if pos.startswith('NN') or pos.startswith('VB')]
+
+        return potential_objects
